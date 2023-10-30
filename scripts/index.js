fa0406f4224401aba476062c632e9e5336643981
Zagudaeva Nataly <zagudaevanatali@yandex.ru>
2023-10-30 23:18:38 +0300

    project 5 iter 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+
 
 // @todo: Функция создания карточки
+function createCard(cardElement, deleteCard) {
+    const card = cardTemplate.querySelector(".card").cloneNode(true),
+        cardImage = card.querySelector('.card__image'),
+        cardTitle = card.querySelector('.card__title'),
+        deleteButton = card.querySelector('.card__delete-button');
+
+    cardImage.src = cardElement.link;
+    cardImage.alt = cardElement.name;
+    cardTitle.textContent = cardElement.name;
+
+    deleteButton.addEventListener("click", deleteCard);
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardItem) =>
+    placesList.append(createCard(cardItem, deleteCard))
+);
+
